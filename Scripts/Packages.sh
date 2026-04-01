cf4dcbf4a4c358a25eb2e1fc90776e4bf918917b
dolphin738 <91973916+dolphin738@users.noreply.github.com>
2026-04-01 23:28:19 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -79,11 +79,11 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 UPDATE_PACKAGE "rtp2httpd" "stackia/rtp2httpd" "main" "" "luci-app-rtp2httpd"
-UPDATE_PACKAGE "dolphin738" "dolphin738/op-packages" "main" "" "luci-app-wrtbwmon  wrtbwmon luci-app-turboacc fast-classifier shortcut-fe luci-app-taskplan luci-app-timedreboot luci-app-netspeedtest homebox ookla-speedtest"
+UPDATE_PACKAGE "dolphin738" "dolphin738/op-packages" "main" "" "luci-app-wrtbwmon  wrtbwmon luci-app-turboacc fast-classifier shortcut-fe luci-app-taskplan luci-app-timedreboot luci-app-netspeedtest homebox ookla-speedtest  dockerd luci-app-dockerman luci-lib-docker"
 UPDATE_PACKAGE "luci-app-netwizard" "sirpdboy/luci-app-netwizard" "main"
 UPDATE_PACKAGE "openwrt-bandix" "timsaya/openwrt-bandix" "main"
 UPDATE_PACKAGE "luci-app-bandix" "timsaya/luci-app-bandix" "main"
-FILE_LIST=("luci-app-wrtbwmon" "wrtbwmon" "luci-app-turboacc" "shortcut-fe" "luci-app-taskplan" "luci-app-timedreboot" "luci-app-netspeedtest" "homebox" "ookla-speedtest")
+FILE_LIST=("luci-app-wrtbwmon" "wrtbwmon" "luci-app-turboacc" "shortcut-fe" "luci-app-taskplan" "luci-app-timedreboot" "luci-app-netspeedtest" "homebox" "ookla-speedtest" "dockerd" "luci-app-dockerman" "luci-lib-docker")
 #mkdir -p files/etc/config/
 for N in "${FILE_LIST[@]}"; do
 	mv -f op-packages/luci/$N ./ 
