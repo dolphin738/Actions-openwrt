49b3f8cbc15fdd2de0aee065848df27ff78d91e0
dolphin738 <91973916+dolphin738@users.noreply.github.com>
2026-04-07 23:32:50 +0800

    Update QCA-feed.sh

--- Scripts/QCA-feed.sh
+++ Scripts/QCA-feed.sh
@@ -2,8 +2,3 @@
 
 #feed修改
 sed -i 's|https://github.com/qosmio/sqm-scripts-nss.git|https://github.com/JuliusBairaktaris/sqm-scripts-nss.git|g' feeds.conf.default
-#sed -i 's|https://github.com/immortalwrt/packages.git|https://github.com/immortalwrt/packages.git;openwrt-25.12|g' feeds.conf.default
-#sed -i 's|https://github.com/immortalwrt/luci.git|https://github.com/immortalwrt/luci.git;openwrt-25.12|g' feeds.conf.default
-#sed -i 's|https://github.com/openwrt/routing.git|https://github.com/openwrt/routing.git;openwrt-25.12|g' feeds.conf.default
-#sed -i 's|https://github.com/openwrt/telephony.git|https://github.com/openwrt/telephony.git;openwrt-25.12|g' feeds.conf.default
-#sed -i 's|https://github.com/openwrt/video.git|https://github.com/openwrt/video.git;openwrt-25.12|g' feeds.conf.default
